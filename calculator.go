2556cc0f8f09ae825447237e83e63d8ed0d536e5
CrissUD <cfpatinoc@correo.udistrital.edu.co>
2020-10-10 19:32:01 -0500

    Se crea modulo de prueba

---
+++ calculator.go
@@ -0,0 +1,42 @@
+package calculatormodule
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type calc struct{}
+
+func (calc) operate(firstValue int, secondValue int, operator string) int {
+	switch operator {
+	case "+":
+		return firstValue + secondValue
+	case "-":
+		return firstValue - secondValue
+	case "*":
+		return firstValue * secondValue
+	case "/":
+		return firstValue / secondValue
+	default:
+		fmt.Println("Error de sintaxis con operador")
+		return 0
+	}
+}
+
+func (calc) toInt(entry string) int {
+	intValue, err := strconv.Atoi(entry)
+	if err == nil {
+		return intValue
+	}
+	fmt.Println("Error de sintaxis con numero")
+	return 0
+}
+
+// ReadIntoKey is...
+func ReadIntoKey() string {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	return scanner.Text()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/CrissUD/module_test_go
+
+go 1.15
